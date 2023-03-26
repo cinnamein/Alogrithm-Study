5ade7d2ad4cd506fa6b0e7a2f8d1832136a9d9b1
윤채민 <yooune.01@gmail.com>
2023-03-27 00:48:07 +0900

    baekjoon 1011 solved

---
+++ Baekjoon/JAVA-1011/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+        for (int i = 0; i < test; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int distance = b - a;
+            int result = 0;
+            int sum = 0;
+            int n = 1;
+            while (distance > sum) {
+                sum += Math.floor((n + 1) / 2);
+                result++;
+                n++;
+            }
+            System.out.println(result);
+        }
+    }
+}
