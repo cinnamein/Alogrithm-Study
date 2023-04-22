5fe7bdac552060b2428a9cb1497cafdc4c4e6592
윤채민 <yooune.01@gmail.com>
2023-04-22 23:19:04 +0900

    baekjoon 15652 solved

---
+++ Baekjoon/JAVA-15649/JAVA-15652/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            dp(i, 1, i + " ");
+        }
+        System.out.println(result);
+    }
+
+    public static void dp(int num, int count, String str) {
+        if (count == m) {
+            result.append(str);
+            result.append("\n");
+            return;
+        }
+        count++;
+        for (int i = num; i <= n; i++) {
+            dp(i, count, str + i + " ");
+        }
+    }
+}
