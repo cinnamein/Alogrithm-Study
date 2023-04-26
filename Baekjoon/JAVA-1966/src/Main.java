ce1539f4fd654f3a6d4f5f50fb743ea1c5a9cb42
yooun <yooune.01@gmail.com>
2023-04-27 02:55:57 +0900

    baekjoon 1966 implementing

--- Baekjoon/JAVA-1966/src/Main.java
+++ Baekjoon/JAVA-1966/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -15,7 +16,7 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
             HashMap<Integer, Integer> map = new HashMap<>();
-            PriorityQueue<Integer> queue = new PriorityQueue<>();
+            PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 int tmp = Integer.parseInt(st.nextToken());
@@ -25,13 +26,14 @@ public class Main {
             int result = n;
             m = map.get(m);
             while (true) {
-                if (m == queue.poll()) {
+                int tmp = queue.poll();
+                if (m == tmp) {
                     break;
                 } else {
-                    result++;
+                    result--;
                 }
             }
-            sb.append(result);
+            sb.append(result + "\n");
         }
         System.out.println(sb);
     }
