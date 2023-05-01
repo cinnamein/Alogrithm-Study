90823fc7e106c64cd8ac8537fccb19a1c3217024
윤채민 <yooune.01@gmail.com>
2023-05-02 00:06:02 +0900

    baekjoon 1991 solved

--- Baekjoon/JAVA-1991/src/Main.java
+++ Baekjoon/JAVA-1991/src/Main.java
@@ -4,6 +4,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class Node {
+        char mid;
+        Node left;
+        Node right;
+
+        Node(char mid, Node left, Node right) {
+            this.mid = mid;
+            this.left = left;
+            this.right = right;
+        }
+    }
     static Node root = new Node('A', null, null);
     static StringBuilder sb = new StringBuilder();
 
@@ -18,24 +29,41 @@ public class Main {
             insertNode(mid, left, right, root);
         }
         preorder(root);
+        sb.append("\n");
         inorder(root);
+        sb.append("\n");
         postorder(root);
         System.out.println(sb);
     }
 
     public static void insertNode(char mid, char left, char right, Node root) {
-//        Node node = new Node(mid, left, right, root);
         if (root.mid == mid) {
-            root.left = ()
+            root.left = (left == '.' ? null : new Node(left, null, null));
+            root.right = (right == '.' ? null : new Node(right, null, null));
+        } else {
+            if (root.left != null) insertNode(mid, left, right, root.left);
+            if (root.right != null) insertNode(mid, left, right, root.right);
         }
     }
 
     public static void preorder(Node root) {
+        if (root == null) return;
+        sb.append(root.mid);
+        preorder(root.left);
+        preorder(root.right);
     }
 
     public static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        sb.append(root.mid);
+        inorder(root.right);
     }
 
     public static void postorder(Node root) {
+        if (root == null) return;
+        postorder(root.left);
+        postorder(root.right);
+        sb.append(root.mid);
     }
 }
