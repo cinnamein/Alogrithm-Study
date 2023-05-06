9e25d8330a957812b7dd2400a27a83e921eaa8ea
윤채민 <yooune.01@gmail.com>
2023-05-06 23:49:23 +0900

    baekjoon 11725 implementing

--- Baekjoon/JAVA-11725/src/Main.java
+++ Baekjoon/JAVA-11725/src/Main.java
@@ -1,27 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, Integer> map = new HashMap<>();
         int n = Integer.parseInt(br.readLine());
-        map.put(1, null);
-        for (int i = 1; i < n; i++) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int j = 1; j <= n; j++) map.put(j, new ArrayList<>(null));
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            if (map.containsKey(x)) {
-                map.put(y, x);
-            }else{
-                map.put(x, y);
-            }
+            map.put(x, new ArrayList<>(map.get(x).add(y)));
+            map.put(y, map.get(y));
         }
         StringBuilder sb = new StringBuilder();
-        for (int j = 2; j <= n; j++) sb.append(map.get(j)).append("\n");
         System.out.println(sb);
     }
 }
