30593fb907e801ccbf993daaf8183c1cdf045029
윤채민 <yooune.01@gmail.com>
2023-05-07 05:24:45 +0900

    baekjoon 11725 implementing

--- Baekjoon/JAVA-11725/src/Main.java
+++ Baekjoon/JAVA-11725/src/Main.java
@@ -10,15 +10,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        for (int j = 1; j <= n; j++) map.put(j, new ArrayList<>(null));
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            map.put(x, new ArrayList<>(map.get(x).add(y)));
-            map.put(y, map.get(y));
-        }
-        StringBuilder sb = new StringBuilder();
-        System.out.println(sb);
+        ArrayList<Integer> list = new ArrayList<>();
+
     }
 }
