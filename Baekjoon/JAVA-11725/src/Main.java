0999d6210b6a4f88f19232f2ab336c0f795cf96d
윤채민 <yooune.01@gmail.com>
2023-05-07 16:30:21 +0900

    baekjoon 11725 solved

--- Baekjoon/JAVA-11725/src/Main.java
+++ Baekjoon/JAVA-11725/src/Main.java
@@ -1,16 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
 
+        ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            tree.add(new ArrayList<>());
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken()) - 1;
+            int n2 = Integer.parseInt(st.nextToken()) - 1;
+            tree.get(n1).add(n2);
+            tree.get(n2).add(n1);
+        }
+
+        boolean[] visited = new boolean[n];
+        int[] parentNode = new int[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        visited[0] = true;
+        while (!queue.isEmpty()) {
+            int v = queue.remove();
+            for (int node : tree.get(v))
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.add(node);
+                    parentNode[node] = v;
+                }
+        }
+        for (int i = 1; i < n; i++)
+            System.out.println(parentNode[i] + 1);
     }
 }
