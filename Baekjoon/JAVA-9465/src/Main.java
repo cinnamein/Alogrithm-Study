ce9b0c765cda562f5d17823cf2f23cf4334e8ac3
윤채민 <yooune.01@gmail.com>
2023-05-14 16:43:13 +0900

    baekjoon 9465 solved

--- Baekjoon/JAVA-9465/src/Main.java
+++ Baekjoon/JAVA-9465/src/Main.java
@@ -9,19 +9,26 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             int m = Integer.parseInt(br.readLine());
-            int[][] arr = new int[2][m];
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[][] arr = new int[2][m + 1];
+            int[][] result = new int[2][m + 1];
             for (int k = 0; k < 2; k++) {
-                for (int j = 0; j < m; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= m; j++) {
                     int tmp = Integer.parseInt(st.nextToken());
                     arr[k][j] = tmp;
                 }
             }
-            int result = 0;
-            for (int r = 0; r < m; r++) {
-                result += Math.max(arr[0][r] + arr[1][r + 2], arr[0][r] + arr[1][r + 1] + arr[0][r + 2]);
+            if (m == 1) System.out.println(Math.max(arr[0][1], arr[1][1]));
+            else if (m == 2) System.out.println(Math.max(arr[0][1] + arr[1][2], arr[1][1] + arr[0][2]));
+            else {
+                result[0][1] = arr[0][1];
+                result[1][1] = arr[1][1];
+                for (int r = 2; r <= m; r++) {
+                    result[0][r] = Math.max(result[1][r - 1], result[1][r - 2]) + arr[0][r];
+                    result[1][r] = Math.max(result[0][r - 1], result[0][r - 2]) + arr[1][r];
+                }
+                System.out.println(Math.max(result[0][m], result[1][m]));
             }
-            System.out.println(result);
         }
     }
 }
