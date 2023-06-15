461d998567dda6e6e35b0bc2621271ae4d1e4545
yooun <yooune.01@gmail.com>
2023-06-16 01:39:23 +0900

    Solved baekjoon 10718 by java

---
+++ Baekjoon/untitled/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "7";
+        int num = Integer.parseInt(str);
+        System.out.println(num + 1);
+
+    }
+
+}
