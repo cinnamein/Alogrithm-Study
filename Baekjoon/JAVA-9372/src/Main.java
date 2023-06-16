23f62c24f2029a00b14fff83ee73904d7d5e3667
윤채민 <yooune.01@gmail.com>
2023-06-16 16:16:51 +0900

    implementing: baekjoon 9372 by JAVA

--- Baekjoon/JAVA-9372/src/Main.java
+++ Baekjoon/JAVA-9372/src/Main.java
@@ -4,20 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class Node {
+        Integer mid;
+        Node left;
+        Node right;
+
+        Node(Integer mid, Node left, Node right) {
+            this.mid = mid;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static Node root = new Node(1, null, null);
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int test = Integer.parseInt(br.readLine());
-        for (int t = 0; t < test; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int[][] city = new int[n + 1][n + 1];
-            for (int k = 0 ; k < m; k++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                city[a][b] = city[b][a] = 1;
-            }
+        while (test-- > 0) {
+            
         }
     }
 }
