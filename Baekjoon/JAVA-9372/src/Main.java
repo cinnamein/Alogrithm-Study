000d30322e4fcfad4c9e42ef66f38f250ab516c9
yooun <yooune.01@gmail.com>
2023-06-18 22:57:08 +0900

    implemented: baekjoon 9372 by JAVA

--- Baekjoon/JAVA-9372/src/Main.java
+++ Baekjoon/JAVA-9372/src/Main.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,23 +12,9 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-            int result = 0;
-            boolean[] city = new boolean[n + 1];
+            int result = n - 1;
             while (m-- > 0) {
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                if (!city[a] && !city[b]) {
-                    city[a] = true;
-                    city[b] = true;
-                    result++;
-                } else if (!city[a]) {
-                    city[a] = true;
-                    result++;
-                } else if (!city[b]) {
-                    city[b] = true;
-                    result++;
-                }
             }
             sb.append(result).append("\n");
         }
