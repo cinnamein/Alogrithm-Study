4c3eb93ca61871d5e1d2be3a02a983636820ed6b
윤채민 <yooune.01@gmail.com>
2023-06-26 16:45:14 +0900

    implemented: baekjoon 9934 solved by python

--- Baekjoon/Python-9934/main.py
+++ Baekjoon/Python-9934/main.py
@@ -1,3 +1,18 @@
 import sys
 
-test = sys.stdin.readline()
+test = int(sys.stdin.readline())
+result = [""] * (2 ** test - 1)
+
+def order(num, length, count):
+    global result
+    if count == test:
+        return
+    tmp = (num + length) // 2
+    result[count] = result[count] + str(arr[tmp]) + " "
+    order(num, tmp - 1, count + 1)
+    order(tmp + 1, length, count + 1)
+
+arr = list(map(int, sys.stdin.readline().split()))
+order(0, len(arr) - 1, 0)
+for k in range(0, test):
+    print(result[k])
