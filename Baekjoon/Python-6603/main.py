3344e80a444328ac8a5048f42bd6dbc303a9d08b
yooun <yooune.01@gmail.com>
2023-06-28 01:14:00 +0900

    solved: baekjoon 6603 by python

--- Baekjoon/Python-6603/main.py
+++ Baekjoon/Python-6603/main.py
@@ -1,16 +1,21 @@
-import sys
-
-def track(sum, num):
-    sum = sum + num + " "
-    for k in range(len(tmp)):
+def track(sum, num, cnt):
+    cnt = cnt + 1
+    sum = sum + str(tmp[num]) + " "
+    if cnt == 6:
+        print(sum)
+        return
+    for k in range(num + 1, len(tmp)):
+        track(sum, k, cnt)
 
 
 while True:
-    global tmp
-    tmp = map(int, sys.stdin.readline().split())
-    if tmp[0] == 0:
-        break
-    global sum
+    global tmp, cnt, sum
+    string = input()
+    if string == "0":
+         break
+    tmp = list(map(int, string.split(" ")))
     sum = ""
-    for i in range(len(tmp)):
-        track(sum, i)
+    cnt = 0
+    for i in range(1, len(tmp)):
+        track(sum, i, cnt)
+    print()
