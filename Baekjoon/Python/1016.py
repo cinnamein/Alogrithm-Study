f04e182c2067671d3a4affb6c38170b1782ada9d
yooun <yooune.01@gmail.com>
2023-07-19 03:26:05 +0900

    implemented: baekjoon 1016 by python

--- Baekjoon/Python/1016.py
+++ Baekjoon/Python/1016.py
@@ -1,16 +1,10 @@
-import math
-
 n, m = map(int, input().split())
-arr = list()
-result = m - n + 1
+arr = [True] * (m - n + 1)
 
-tmp = int(math.sqrt(m) + 1)
+for i in range(2, int(m ** 0.5) + 1):
+    tmp = i ** 2
+    for ii in range(n // tmp * tmp, m + 1, tmp):
+        if ii - n >= 0 and arr[ii - n]:
+            arr[ii - n] = False
 
-for i in range(2, tmp):
-    arr.append(i * i)
-
-for k in range(n, m + 1):
-    if any(k % prime == 0 for prime in arr):
-        result -= 1
-
-print(result)
+print(arr.count(True))
