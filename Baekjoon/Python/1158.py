bbdf631bf10d0a38bd04813221bae880061fd3cc
yooun <yooune.01@gmail.com>
2023-09-05 20:59:52 +0900

    implemented: baekjoon 1158 by python

--- Baekjoon/Python/1158.py
+++ Baekjoon/Python/1158.py
@@ -1,6 +1,12 @@
+from collections import deque
+
 n, m = map(int, input().split())
-queue = list()
-
+tmp = deque()
 for i in range(1, n + 1):
-    queue.append(i)
-
+    tmp.append(i)
+res = [] * n
+while tmp:
+    for _ in range(m - 1):
+        tmp.append(tmp.popleft())
+    res.append(tmp.popleft())
+print(str(res).replace('[', '<').replace(']', '>'))
