ed1f6180f63b5f9a542d403b4c5fdcd0b5af9c90
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-09-25 15:13:44 +0900

    Solved: baekjoon 1725 by python

--- Baekjoon/Python/1725.py
+++ Baekjoon/Python/1725.py
@@ -1,19 +1,18 @@
 import sys
 
 n = int(sys.stdin.readline())
-if n == 1:
-    print(sys.stdin.readline())
-else:
-    arr = [int(sys.stdin.readline()) for _ in range(n)]
-    stack = []
-    res = 0
-    for i in range(n):
-        height = i
-        while stack and stack[-1][1] >= arr[i]:
-            height = stack[-1][0]
-            width = i - height
-            res = max(res, width * stack[-1][1])
-            stack.pop()
-        stack.append((height, arr[i]))
-    print(res)
+arr = [int(sys.stdin.readline()) for _ in range(n)]
+arr.append(0)
+stack = [[0, arr[0]]]
+res = 0
 
+if n == 1:
+    print(arr[0])
+else:
+    for i in range(1, n + 1):
+        cursor = i
+        while stack and stack[-1][1] > arr[i]:
+            cursor, tmp = stack.pop()
+            res = max(res, tmp * (i - cursor))
+        stack.append([cursor, arr[i]])
+    print(res)
