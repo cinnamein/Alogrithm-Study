9aa99a49f61b88070b9ec51f865623d612ada9f8
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-09-28 18:47:14 +0900

    Solved: baekjoon 14940 by python

--- Baekjoon/Python/14940.py
+++ Baekjoon/Python/14940.py
@@ -1,33 +1,44 @@
 import sys
+from collections import deque
 
 n, m = map(int, sys.stdin.readline().split())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-res = [[-1] * m] * n
-vis = [[False] * m] * n
-a, b = 0, 0
-mx = [0, 1, 0, -1]
-my = [1, 0, -1, 0]
-for y in range(n):
-    for x in range(m):
-        if arr[y][x] == 2:
-            a = x
-            b = y
-vis[b][a] = True
+input_arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+result_arr = [[-1 for _ in range(m)] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+qu = deque()
+mx, my = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+# 2 찾기
+for i in range(n):
+    for ii in range(m):
+        if input_arr[i][ii] == 2:
+            startX = ii
+            startY = i
+        elif input_arr[i][ii] == 0:
+            result_arr[i][ii] = 0
+            visited[i][ii] = True
 
 
-def bfs(tmpx, tmpy):
+def bfs(curX, curY):
+    # 다음 이동 칸
     for i in range(4):
-        y = tmpy + my[i]
-        x = tmpx + mx[i]
-        if 0 <= y < n and 0 <= x < m:
-            if arr[y][x] == 0:
-                res[y][x] = 0
-                return
-            elif res[y][x] == -1:
-                res[y - 1][x] = res[y][x] + 1
-    for i in range(4):
-        bfs(tmpx + mx[i], tmpy + my[i])
+        nextX = curX + mx[i]
+        nextY = curY + my[i]
+        # 방문하지 않은 칸이고, 범위 안의 좌표일 때만 실행
+        if 0 <= nextX < m and 0 <= nextY < n and not visited[nextY][nextX]:
+            result_arr[nextY][nextX] = result_arr[curY][curX] + 1
+            visited[nextY][nextX] = True
+            qu.append([nextX, nextY])
 
 
-bfs(a, b)
-print(res)
+result_arr[startY][startX] = 0
+visited[startY][startX] = True
+qu.append([startX, startY])
+while len(qu) != 0:
+    tmp = qu.popleft()
+    bfs(tmp[0], tmp[1])
+
+for i in range(n):
+    for ii in range(m):
+        print(result_arr[i][ii], end=' ')
+    print()
