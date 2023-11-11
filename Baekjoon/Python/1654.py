efd47d59e732a324a0991ba562741355ff038546
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-11-12 00:45:13 +0900

    Solved: baekjoon 1654 by python

--- Baekjoon/Python/1654.py
+++ Baekjoon/Python/1654.py
@@ -2,23 +2,23 @@ import sys
 
 k, n = map(int, sys.stdin.readline().split())
 lan = [0] * k
-low = 1
+low = 0
 high = 2147483647
+res = 0
 
 for i in range(k):
     tmp = int(sys.stdin.readline())
     lan[i] = tmp
 
-while low != high:
-    if high == low:
-        break
-    sum = 0
+while low <= high:
     mid = (high + low) // 2
+    sum = 0
     for i in lan:
         sum += i // mid
     if sum >= n:
         low = mid + 1
+        res = mid
     elif sum < n:
         high = mid - 1
 
-print(high - 1)
+print(res)
